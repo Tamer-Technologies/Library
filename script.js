90b83546754ce4dd882f23ac8fa455a7af9818f0
Tamer Technologies <216339134+Tamer-Technologies@users.noreply.github.com>
2025-06-30 06:17:54 +0300

    feat: Implement 'My Library' display
    Adds the foundational HTML structure for the book grid, JavaScript logic
    for dynamic book rendering, and associated CSS for styling the library
    section and individual book cards.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+const booksContainer = document.querySelector(".books-grid");
+
 const MyLibrary = [];
+
+
 
 function Book(id, title, img, author, year) {
   this.id = id;
@@ -13,6 +17,22 @@ function addBookToLibrary(title, img, author, year) {
   const book = new Book(uniqueId, title, img, author, year);
   MyLibrary.push(book);
 }
+
+
+MyLibrary.forEach(book => {
+  const bookElement = document.createElement('div');
+  bookElement.className = 'book';
+  bookElement.innerHTML = `
+      <div class="book-cover">
+      <img src="/media/unknown-cover.jpg" alt="${book.title}">
+      </div>
+      <h4 class="book-title">${book.title}</h4>
+      <p class="book-author">${book.author}</p>
+      <p class="book-year">${book.year}</p>
+  `;
+  booksContainer.appendChild(bookElement);
+
+})
 
 
 
